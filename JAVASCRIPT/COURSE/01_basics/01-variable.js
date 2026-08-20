710bc6baa6c0932e32af925ae9f8f15c12867fb3
justtpriya-12 <priyachauhan51574@gmail.com>
2026-08-21 01:48:37 +0530

    ways to declare variables

--- JAVASCRIPT/COURSE/01_basics/01-variable.js
+++ JAVASCRIPT/COURSE/01_basics/01-variable.js
@@ -1,8 +1,11 @@
-const accountId = 144553 
-let accountEmail = "priyachauhan51574gmail.com"
+//constant declare through const
+const accountId = 144553     //can't change  value
+
+//variable declare through let and var
+let accountEmail = "priyachauhan51574gmail.com"  //change value
 var accountPassword = "12345"
-accountCity = "Mathura"
-let accountState;
+accountCity = "Mathura"   //it works but not a good way
+let accountState;   //declare but not assigned any value , it show undefined 
 
 //accountId = 2  //not allowed const var can not reassign
 console.log(accountId);
